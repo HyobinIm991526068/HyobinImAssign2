2e0f52c7986fe3b33638dd02c07ddee629822039
ihb92 <jakeim9@gmail.com>
2021-02-27 01:23:46 -0500

    Added all different pages and started working on menu in the main page

--- HyobinAssign22/src/main/java/hyobin/im/s991526068/HyobinActivity.java
+++ HyobinAssign22/src/main/java/hyobin/im/s991526068/HyobinActivity.java
@@ -2,7 +2,12 @@ package hyobin.im.s991526068;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HyobinActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class HyobinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        Intent intent = null;
+        //Handle item selection
+        switch (item.getItemId()){
+            case R.id.help:
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(getString(R.string.site_label)));
+                startActivity(intent);
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
